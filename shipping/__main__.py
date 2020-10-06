c201f7818a09217849843e469f07aba116224235
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-10-06 10:02:48 +0200

    Black fixes

--- shipping/__main__.py
+++ shipping/__main__.py
@@ -3,17 +3,17 @@ shipping.__main__
 
 The main entry point for the command line interface.
 
-Invoke as ``ship`` (if installed)
-or ``python -m shio`` (no install required).
+Invoke as ``shipping`` (if installed)
+or ``python -m shipping`` (no install required).
 """
 import sys
 
 from shipping.cli.base import cli
 
+
 def main():
     sys.exit(cli())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-
