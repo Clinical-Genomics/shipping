219644f573c7959d9a172d329bee447ff034d56f
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-10-07 07:55:03 +0200

    Delete env first in test

--- tests/cli/test_check_cmd.py
+++ tests/cli/test_check_cmd.py
@@ -5,17 +5,19 @@ import logging
 from click.testing import CliRunner
 
 from shipping.cli.check import check_cmd
-from shipping.environment import conda_exists, get_conda_path
+from shipping.commands import Process
+from shipping.environment import conda_exists, delete_conda_env, get_conda_path
 
 
 def test_check_cmd_no_environment(env_name: str, context: dict, caplog):
     """Test to run the check command when the environment does not exist"""
     caplog.set_level(logging.DEBUG)
     # GIVEN a environment that does not exist
+    delete_conda_env(Process("conda"), env_name)
     assert conda_exists(get_conda_path(env_name)) is False
     # GIVEN a cli runner
-    # GIVEN a context with basic information
     runner = CliRunner()
+    # GIVEN a context with basic information
 
     # WHEN running the command to check if deployment is possible
     runner.invoke(check_cmd, [], obj=context)
