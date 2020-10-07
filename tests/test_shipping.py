213ddbae8b5f1c0f85d5632fe3245cbf6354032f
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-10-07 19:51:20 +0200

    Removes version test

--- tests/test_shipping.py
+++ tests/test_shipping.py
@@ -2,10 +2,6 @@ from shipping import __version__
 from shipping.commands import Process
 
 
-def test_version():
-    assert __version__ == "0.1.0"
-
-
 def test_run_main():
     python_process = Process("python")
     call = ["-m", "shipping", "--version"]
