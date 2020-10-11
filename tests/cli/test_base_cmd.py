5965ff32a48fb9c784a8d044e551f2c5ea53f590
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-10-11 09:35:36 +0200

    Adds test when host if wrong

--- tests/cli/test_base_cmd.py
+++ tests/cli/test_base_cmd.py
@@ -1,5 +1,6 @@
 """Tests for the base command"""
 import logging
+from pathlib import Path
 
 from click.testing import CliRunner
 
@@ -42,3 +43,20 @@ def test_run_shipping_no_tool(caplog):
     assert result.exit_code != 0
     # THEN assert that correct information is printed
     assert "Please provide either app config or tool" in caplog.text
+
+
+def test_run_shipping_wrong_host(faulty_host_config: Path, tool_name: str, caplog):
+    """Test to run shipping with a host config where the host is not the same as current host"""
+    caplog.set_level(logging.DEBUG)
+    # GIVEN a cli runner and the base command
+    runner = CliRunner()
+
+    # WHEN running the check command
+    result = runner.invoke(
+        cli, ["--host-config", str(faulty_host_config), "--tool-name", tool_name, "check"]
+    )
+
+    # THEN assert it exits with a non zero exit code
+    assert result.exit_code != 0
+    # THEN assert that correct information is printed
+    assert "is not same as config host" in caplog.text
